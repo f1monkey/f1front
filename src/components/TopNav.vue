fa55c57c5ce72a6189879cb23629d78652c697ed
cyradin <cyradin@nextmail.ru>
2019-06-16 02:42:45 +0300

    remove menu item select handler

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-menu :default-active="$route.path" :router="true" mode="horizontal" @select="handleSelect">
+  <el-menu :default-active="$route.path" :router="true" mode="horizontal">
     <el-menu-item index="/">Main</el-menu-item>
     <el-menu-item index="/options">Options</el-menu-item>
   </el-menu>
@@ -11,12 +11,5 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component({
   components: {},
 })
-export default class TopNav extends Vue {
-  activeIndex: string = '1';
-
-  handleSelect(key: string, keyPath: string) {
-    console.dir(this.$route.path);
-    console.log(key, keyPath);
-  }
-}
+export default class TopNav extends Vue {}
 </script>
