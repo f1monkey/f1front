01918077d27eef005263de682d2cba852429e3c5
cyradin <cyradin@nextmail.ru>
2019-06-23 18:20:41 +0300

    add separate component for options

--- src/views/Options.vue
+++ src/views/Options.vue
@@ -1,39 +1,15 @@
 <template>
-  <div>
-    <el-row>
-      <el-col :span="5">
-        <el-menu :default-active="currentGroup" @select="handleSelect">
-          <el-menu-item index="characters">
-            <i class="el-icon-user-solid"></i>
-            <span>Characters</span>
-          </el-menu-item>
-        </el-menu>
-      </el-col>
-      <el-col :span="19">
-        <el-main>
-          <div v-if="currentGroup === 'characters'">
-            <characters></characters>
-          </div>
-        </el-main>
-      </el-col>
-    </el-row>
-  </div>
+  <options></options>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import Characters from '@/components/Options/Characters/Characters.vue';
+import Options from '@/components/Options/Options.vue';
 
 @Component({
   components: {
-    Characters,
+    Options,
   },
 })
-export default class OptionsView extends Vue {
-  protected currentGroup: string = 'characters';
-
-  protected handleSelect(selectedGroup: string) {
-    this.currentGroup = selectedGroup;
-  }
-}
+export default class OptionsView extends Vue {}
 </script>
