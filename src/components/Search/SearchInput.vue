4cae709e94ae40e5a6fea13077d43cc69c219a4a
cyradin <cyradin@nextmail.ru>
2019-06-23 09:57:29 +0300

    remove autocompletion from search input component

--- src/components/Search/SearchInput.vue
+++ src/components/Search/SearchInput.vue
@@ -1,13 +1,11 @@
 <template>
-  <el-autocomplete
-    class="inline-input"
+  <el-input
     v-model="query"
-    :fetch-suggestions="querySearch"
+    prefix-icon="el-icon-search"
     placeholder="Search..."
-    :trigger-on-focus="true"
-    @select="handleSelect"
     size="medium"
-  ></el-autocomplete>
+    clearable
+  ></el-input>
 </template>
 
 <script lang="ts">
@@ -17,47 +15,12 @@ import { Component, Vue } from 'vue-property-decorator';
   components: {},
 })
 export default class SearchInput extends Vue {
-  protected links = [
-    { value: 'vue', link: 'https://github.com/vuejs/vue' },
-    { value: 'element', link: 'https://github.com/ElemeFE/element' },
-    { value: 'cooking', link: 'https://github.com/ElemeFE/cooking' },
-    { value: 'mint-ui', link: 'https://github.com/ElemeFE/mint-ui' },
-    { value: 'vuex', link: 'https://github.com/vuejs/vuex' },
-    { value: 'vue-router', link: 'https://github.com/vuejs/vue-router' },
-    { value: 'babel', link: 'https://github.com/babel/babel' },
-  ];
+  get query() {
+    return this.$store.state.search.query;
+  }
 
-  protected query: string = '';
-
-  querySearch(queryString: string, cb: any) {
-    let links = this.links;
-    let results = queryString
-      ? links.filter(this.createFilter(queryString))
-      : links;
-    // call callback function to return suggestions
-    cb(results);
-  }
-  createFilter(queryString: string) {
-    return (link: any) => {
-      return link.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0;
-    };
-  }
-  loadAll() {
-    return [
-      { value: 'vue', link: 'https://github.com/vuejs/vue' },
-      { value: 'element', link: 'https://github.com/ElemeFE/element' },
-      { value: 'cooking', link: 'https://github.com/ElemeFE/cooking' },
-      { value: 'mint-ui', link: 'https://github.com/ElemeFE/mint-ui' },
-      { value: 'vuex', link: 'https://github.com/vuejs/vuex' },
-      { value: 'vue-router', link: 'https://github.com/vuejs/vue-router' },
-      { value: 'babel', link: 'https://github.com/babel/babel' },
-    ];
-  }
-  handleSelect(item: any) {
-    console.log(item);
-  }
-  mounted() {
-    this.links = this.loadAll();
+  set query(query: string) {
+    this.$store.dispatch('search/setQuery', query);
   }
 }
 </script>
